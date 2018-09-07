d9667ca82841af209e6dcb476a0bf6f69c5f19a2
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2018-09-07 22:36:57 +0200

    Add training dataset with a couple of warming up trainings

--- src/generate.js
+++ src/generate.js
@@ -1,6 +1,30 @@
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function generateTraining(attentionPoint, swimTraining, swimSpeed) {
-    return "This is a new training with " + attentionPoint
-        + " " + swimTraining + " " + swimSpeed + ".";
+    var totalDistance = 0;
+
+    if (swimSpeed) {
+        var data = trainingdb({'training_part': 'warming_up', 'swim_speed': swimSpeed});
+        var randomInt = getRandomInt(0, data.count() - 1);
+        var warmingUp = data.get()[randomInt].training_text;
+        totalDistance += data.get()[randomInt].distance;
+
+        console.log(warmingUp, totalDistance);
+    }
+
+    var trainingText = "";
+
+    if (warmingUp) {
+        trainingText += "<h2>Warming Up</h2>" + "<p>" +  warmingUp + "</p>";
+    }
+
+    return trainingText;
 }
 
 $(document).ready(function() {
---
+++ src/trainingdb.js
@@ -0,0 +1,50 @@
+var trainingdb = TAFFY([
+    {
+        "training_part": "warming_up",
+        "attention_point": null,
+        "training_type": null,
+        "swim_speed": "novice",
+        "training_text": "200m freestyle",
+        "distance": 200
+    },
+    {
+        "training_part": "warming_up",
+        "attention_point": null,
+        "training_type": null,
+        "swim_speed": "intermediate",
+        "training_text": "400m freestyle",
+        "distance": 400
+    },
+    {
+        "training_part": "warming_up",
+        "attention_point": null,
+        "training_type": null,
+        "swim_speed": "advanced",
+        "training_text": "2 x 400m freestyle",
+        "distance": 800
+    },
+    {
+        "training_part": "warming_up",
+        "attention_point": null,
+        "training_type": null,
+        "swim_speed": "novice",
+        "training_text": "50m breaststroke, 50m freestyle, 50m backstroke, 50m freestyle",
+        "distance": 200
+    },
+    {
+        "training_part": "warming_up",
+        "attention_point": null,
+        "training_type": null,
+        "swim_speed": "intermediate",
+        "training_text": "100m breaststroke, 100m freestyle, 100m backstroke, 100m freestyle",
+        "distance": 400
+    },
+    {
+        "training_part": "warming_up",
+        "attention_point": null,
+        "training_type": null,
+        "swim_speed": "advanced",
+        "training_text": "2 x (100m breaststroke, 100m freestyle, 100m backstroke, 100m freestyle)",
+        "distance": 800
+    },
+]);
